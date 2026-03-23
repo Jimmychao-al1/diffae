d481559b27502da979e619b41ac30e77960b6920
Jimmychao-al1 <jimmychao0803@gmail.com>
2026-03-23 17:27:00 +0800

    update

--- QATcode/quantize_ver2/downsample_talsq_ckpt.py
+++ QATcode/quantize_ver2/downsample_talsq_ckpt.py
@@ -2,7 +2,7 @@ import torch
 ckpt = torch.load('QATcode/quantize_ver2/checkpoints/diffae_step6_lora_best.pth', map_location='cpu')
 newsd = {}
 for k, v in ckpt.items():
-    if 'delta_list' in k or 'zp_list' in k:
+    if 'scale_list' in k or 'zp_list' in k:
         # newv = v[::12][:20]
         newv = v[::5]
     
